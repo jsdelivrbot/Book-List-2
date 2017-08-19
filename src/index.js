02a368182917fede71738dbcc86582f05828e011
Bar Maltzman <barmal8@gmail.com>
2017-08-20 00:36:14 +0300

    the App

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import App from "./components/app";
+import reducers from "./reducers";
+
+//const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector(".container")
+);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'A journey to the end of the night', pages: 101},
+        {title: 'The picture of dorian grey', pages: 39},
+        {title: 'The master and Margarita', pages: 83},
+        {title: 'Jim Morrison Biography', pages: 34},
+    ]
+}
